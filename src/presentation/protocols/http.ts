8db698793ca505c08197bbcb2240d823c45aab91
Iago Maia <iago.maiasilva@gmail.com>
2022-01-22 14:56:21 -0300

    refactor: remove generic type of protocols

--- src/presentation/protocols/http.ts
+++ src/presentation/protocols/http.ts
@@ -1,8 +1,8 @@
-export interface HttpResponse<T> {
+export interface HttpResponse {
   statusCode: number
-  body: T
+  body: any
 }
 
-export interface HttpRequest<T> {
-  body?: T
+export interface HttpRequest {
+  body?: any
 }
