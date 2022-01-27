4823acdd779720559c13ecc265b62e0145837e12
Iago Maia <iago.maiasilva@gmail.com>
2022-01-27 18:24:28 -0300

    refactor: move new EmailValidatorAdapter to a factory method

--- src/utils/email-validator.adapter.spec.ts
+++ src/utils/email-validator.adapter.spec.ts
@@ -7,20 +7,24 @@ jest.mock('validator', () => ({
   }
 }))
 
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
 describe('EmailValidatorAdapter', () => {
   test('Should return false if validator returns false', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email')
     expect(isValid).toBe(false)
   })
   test('Should return true if validator returns true', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isValid = sut.isValid('valid_email@email.com')
     expect(isValid).toBe(true)
   })
   test('Should call validator with correct value', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isValidSpy = jest.spyOn(validator, 'isEmail')
     sut.isValid('valid_email@email.com')
     expect(isValidSpy).toHaveBeenCalledWith('valid_email@email.com')
