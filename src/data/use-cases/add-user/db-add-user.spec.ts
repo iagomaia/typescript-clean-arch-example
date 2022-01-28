d0f33edc3bd8fdde1e85261228e38b53015466d0
Iago Maia <iago.maiasilva@gmail.com>
2022-01-27 22:02:45 -0300

    test: ensure DbAddUser throws if Encrypter throws

--- src/data/use-cases/add-user/db-add-user.spec.ts
+++ src/data/use-cases/add-user/db-add-user.spec.ts
@@ -38,4 +38,21 @@ describe('DbAddUser UseCase', () => {
     await sut.add(userDto)
     expect(encryptSpy).toHaveBeenCalledWith('valid_password')
   })
+  test('Should throw if encrypter throws', async () => {
+    const {
+      sut,
+      encrypterStub
+    } = makeSut()
+    jest.spyOn(encrypterStub, 'encrypt')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      )
+    const userDto = {
+      name: 'Valid Name',
+      email: 'valid_email@email.com',
+      password: 'valid_password'
+    }
+    const promise = sut.add(userDto)
+    await expect(promise).rejects.toThrow()
+  })
 })
